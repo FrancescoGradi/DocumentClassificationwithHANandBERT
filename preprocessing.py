21b88907a0bb6bcbf96eb2efe107753d98c6af6d
FrancescoGradi <gradifrancesco@yahoo.it>
2020-05-24 16:27:47 +0200

    Preprocessing della rete

--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,15 @@
 import string
 import pickle
+import os.path
 
 import pandas as pd
+import numpy as np
 import tensorflow_datasets as tfds
 
 from keras.preprocessing.text import Tokenizer, text_to_word_sequence
 from nltk.corpus import stopwords
 
-from utils import cleanString
+from utils import cleanString, splitDataframe, wordToSeq, toCategorical
 
 
 MAX_FEATURES = 200000  # maximum number of unique words that should be included in the tokenized word index
@@ -19,13 +21,15 @@ EMBED_SIZE = 100  # vector size of word embedding
 if __name__ == '__main__':
     '''
     # Reading JSON dataset with Pandas
-
-    data_df = pd.read_json("IMDB.json")
+    
+    dataset_name = "imdb_complete"
+    data_df = pd.read_json(dataset_name + "json")
     data_df = data_df[["rating", "review"]]
     data_df.columns = ["label", "text"]
     '''
 
-    ds = tfds.load('imdb_reviews', split='train')
+    dataset_name = 'imdb_reviews'
+    ds = tfds.load(dataset_name, split='train')
     reviews = []
     for element in ds.as_numpy_iterator():
         reviews.append((element['text'].decode('utf-8'), element['label']))
@@ -56,16 +60,65 @@ if __name__ == '__main__':
 
     print(data_cleaned)
 
-    # We construct a word index that associates a word to a integer number, and it is saved
+    # We construct a word index that associates a word to a integer number and it is saved
 
-    texts = []
-    n = data_cleaned['text'].shape[0]
-    for i in range(n):
-        s = data_cleaned['text'].iloc[i]
-        s = ' '.join([word.strip(string.punctuation) for word in s.split() if word.strip(string.punctuation) is not ""])
-        texts.append(s)
-    tokenizer = Tokenizer(num_words=MAX_FEATURES, lower=True, oov_token=None)
-    tokenizer.fit_on_texts(texts)
-    word_index = tokenizer.word_index
-    with open('word_index.txt', 'wb') as f:
-        pickle.dump(word_index, f)
+    if os.path.isfile('indices/word_index_' + dataset_name + '.txt'):
+        with open('indices/word_index_' + dataset_name + '.txt', 'rb') as f:
+            word_index = pickle.load(f)
+    else:
+        texts = []
+        n = data_cleaned['text'].shape[0]
+        for i in range(n):
+            s = data_cleaned['text'].iloc[i]
+            s = ' '.join([word.strip(string.punctuation) for word in s.split() if word.strip(string.punctuation) is not ""])
+            texts.append(s)
+        tokenizer = Tokenizer(num_words=MAX_FEATURES, lower=True, oov_token=None)
+        tokenizer.fit_on_texts(texts)
+        word_index = tokenizer.word_index
+        os.makedirs(os.path.dirname('indices/word_index_' + dataset_name + '.txt'), exist_ok=True)
+        with open('indices/word_index_' + dataset_name + '.txt', 'wb') as f:
+            pickle.dump(word_index, f)
+
+    # We read a pre-trained dataset (Glove) that contains a words list: every word is associated with a numeric vector
+    # of dim 100 (in this implementation). It is downloadable at https://github.com/stanfordnlp/GloVe
+
+    embeddings_index = {}
+    with open(os.path.join(os.getcwd(), 'glove.6B.100d.txt'), encoding='UTF-8') as f:
+        for line in f:
+            values = line.split()
+            embeddings_index[values[0]] = np.asarray(values[1:], dtype='float32')
+
+    # We create an embedding matrix for the network
+
+    embedding_matrix = np.zeros((len(word_index) + 1, EMBED_SIZE))
+    for word, i in word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+
+
+    train, validation, test = splitDataframe(data_cleaned, 'code', 0.8, 0.1, 0.1)
+
+    # Training
+    paras = []
+    for i in range(train['text'].shape[0]):
+        sequence = wordToSeq(train['text'].iloc[i], word_index, MAX_SENTENCE_NUM, MAX_WORD_NUM, MAX_FEATURES)
+        paras.append(sequence)
+    x_train = np.array(paras)
+    y_train = toCategorical(train['code'], categoryToCode)
+
+    # Validation
+    paras = []
+    for i in range(validation['text'].shape[0]):
+        sequence = wordToSeq(validation['text'].iloc[i], word_index, MAX_SENTENCE_NUM, MAX_WORD_NUM, MAX_FEATURES)
+        paras.append(sequence)
+    x_val = np.array(paras)
+    y_val = toCategorical(validation['code'], categoryToCode)
+
+    # Test
+    paras = []
+    for i in range(test['text'].shape[0]):
+        sequence = wordToSeq(test['text'].iloc[i], word_index, MAX_SENTENCE_NUM, MAX_WORD_NUM, MAX_FEATURES)
+        paras.append(sequence)
+    x_test = np.array(paras)
+    y_test = toCategorical(test['code'], categoryToCode)
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from nltk import tokenize
 from nltk.stem import WordNetLemmatizer
 
+import numpy as np
+
 
 def cleanString(text, stop_words):
     """
@@ -26,3 +28,91 @@ def cleanString(text, stop_words):
         cleaned_string += ' '.join(word_list) + ' . '
 
     return cleaned_string
+
+
+def splitDataframe(dataframe, column_name, training_split=0.6, validation_split=0.2, test_split=0.2):
+    """
+    Splits a pandas dataframe into trainingset, validationset and testset in specified ratio.
+    All sets are balanced, which means they have the same ratio for each categorie as the full set.
+    Input:   dataframe        - Pandas Dataframe, should include a column for data and one for categories
+             column_name      - Name of dataframe column which contains the categorical output values
+             training_split   - from ]0,1[, default = 0.6
+             validation_split - from ]0,1[, default = 0.2
+             test_split       - from ]0,1[, default = 0.2
+                                Sum of all splits need to be 1
+    Output:  train            - Pandas DataFrame of trainset
+             validation       - Pandas DataFrame of validationset
+             test             - Pandas DataFrame of testset
+    """
+    if training_split + validation_split + test_split != 1.0 and training_split > 0 and validation_split > 0 and \
+            test_split > 0:
+        raise ValueError('Split paramter sum should be 1.0')
+
+    total = len(dataframe.index)
+
+    train = dataframe.reset_index().groupby(column_name).apply(lambda x: x.sample(frac=training_split)) \
+        .reset_index(drop=True).set_index('index')
+    train = train.sample(frac=1)
+    temp_df = dataframe.drop(train.index)
+    validation = temp_df.reset_index().groupby(column_name) \
+        .apply(lambda x: x.sample(frac=validation_split / (test_split + validation_split))) \
+        .reset_index(drop=True).set_index('index')
+    validation = validation.sample(frac=1)
+    test = temp_df.drop(validation.index)
+    test = test.sample(frac=1)
+
+    return train, validation, test
+
+
+def wordToSeq(text, word_index, max_sentences, max_words, max_features):
+    """
+    Converts a string to a numpy matrix where each word is tokenized.
+    Arrays are zero-padded to max_sentences and max_words length.
+
+    Input:    text           - string of sentences
+              word_index     - trained word_index
+              max_sentences  - maximum number of sentences allowed per document for HAN
+              max_words      - maximum number of words in each sentence for HAN
+              max_features   - maximum number of unique words to be tokenized
+    Output:   data           - Numpy Matrix of size [max_sentences x max_words]
+    """
+    sentences = tokenize.sent_tokenize(text)
+    data = np.zeros((max_sentences, max_words), dtype='int32')
+    for j, sent in enumerate(sentences):
+        if j < max_sentences:
+            wordTokens = tokenize.word_tokenize(sent.rstrip('.'))
+            wordTokens = [w for w in wordTokens]
+            k = 0
+            for _, word in enumerate(wordTokens):
+                try:
+                    if k < max_words and word_index[word] < max_features:
+                        data[j, k] = word_index[word]
+                        k = k + 1
+                except:
+                    pass
+    return data
+
+
+def toCategorical(series, class_dict):
+    """
+    Converts category labels to vectors,
+    Input:     series     - pandas Series containing numbered category labels
+               class_dict - dictionary of integer to category string
+                            e.g. {0: 'business', 1: 'entertainment', 2: 'politics', 3: 'sport', 4: 'tech'}
+    Output:    Array      - numpy array containing categories converted to lists
+                            e.g. 0:'business'      -> [1 0 0 0 0]
+                                 1:'entertainment' -> [0 1 0 0 0]
+                                 2:'politics'      -> [0 0 1 0 0]
+                                 3:'sport'         -> [0 0 0 1 0]
+                                 4:'tech'          -> [0 0 0 0 1]
+    """
+    n_classes = len(class_dict)
+    new_dict = {}
+    for key, value in class_dict.items():
+        cat_list = [0] * n_classes
+        cat_list[key] = 1
+        new_dict[key] = cat_list
+    y_cat = []
+    for key, value in series.iteritems():
+        y_cat.append(new_dict[value])
+    return np.array(y_cat)
