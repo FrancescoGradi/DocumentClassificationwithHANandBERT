b82dc17f6dc5410817fd334d825e51ea8fbf2d67
FrancescoGradi <gradifrancesco@yahoo.it>
2020-06-25 22:13:39 +0200

    Correzione soft targets e nuova evaluate

--- README.md
+++ README.md
@@ -51,7 +51,7 @@ papers results.
 | HAN         | 86.6       | 42.5 | 69.0      |
 | BERT_base   | 94.6       | 57.7 | 77.4      |
 | LSTM_reg    | 94.2       | 52.7 | 71.1      |
-| KD-LSTM_reg | 94.6       | -    | -         |
+| KD-LSTM_reg | 94.6       | -    | 71.7      |
 
 ## Visualization of Attention in HAN
 
--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from utils import readImdbSmall, readIMDB, readYelp
 if __name__ == '__main__':
 
     # dataset_name, n_classes, data_df = readImdbSmall()
-    # dataset_name, n_classes, data_df = readIMDB()
-    dataset_name, n_classes, data_df = readYelp()
+    dataset_name, n_classes, data_df = readIMDB()
+    # dataset_name, n_classes, data_df = readYelp()
 
     # lstmTrain(dataset_name, n_classes)
     # lstmEvaluate(dataset_name, n_classes, model_path='models/model_imdb_reviews_lstm/ckp_12epochs_20200618-133908', isCheckpoint=True)
 
-    # kdPreprocessing(dataset_name, n_classes, data_df, teacher_path='models/model_yelp_2014_bert/20200607-201214', MAX_LEN=128)
-    # softTargetsEvaluate(dataset_name, n_classes)
+    kdPreprocessing(dataset_name, n_classes, data_df, teacher_path='models/model_IMDB_bert/20200605-184848', MAX_LEN=128)
+    softTargetsEvaluate(dataset_name, n_classes)
     kdLstmTrain(dataset_name, n_classes, EPOCHS=20, TRAIN_BATCH_SIZE=64, LAMBDA=1)
-    # lstmEvaluate(dataset_name, n_classes, model_path='models/model_imdb_reviews_kdLstm/ckp_17epochs_20200618-222526', isCheckpoint=True)
+    # lstmEvaluate(dataset_name, n_classes, model_path='models/model_yelp_2014_kdLstm/ckp_18epochs_20200625-212907', isCheckpoint=True)
 
     # hanPreprocessing(dataset_name, data_df, save_all=True, cleaned=True, MAX_WORD_NUM=25, MAX_SENTENCE_NUM=15, EMBED_SIZE=100)
     # hanTrain(dataset_name, n_classes, MAX_WORD_NUM=25, MAX_SENTENCE_NUM=15, EMBED_SIZE=100)
